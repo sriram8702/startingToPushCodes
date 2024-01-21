0f7e5a3eec4789a13356480d717644c9baca6f58
sriram8702 <sriram8702@gmail.com>
2024-01-21 12:46:21 +0530

    Dynamic routes and params 1

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
+const errorController = require('./controllers/error');
 
-const errorController=require('./controllers/error')
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoute = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoute);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
--- routes/admin.js
+++ routes/admin.js
@@ -2,20 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir=require('../path.js')
-
-const productController=require('../controllers/product');
-const contactController=require('../controllers/contact.js')
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', productController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
-router.get("/contactus",contactController.contactform)
-
-router.post("/sucess",contactController.sucess)
 module.exports = router;
