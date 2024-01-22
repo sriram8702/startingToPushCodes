29c651dd9f6e3321955713d4e4db53a029da1e74
sriram8702 <sriram8702@gmail.com>
2024-01-22 17:34:45 +0530

    Edit and delete functionality of shop

--- models/product.js
+++ models/product.js
@@ -17,8 +17,10 @@ const getProductsFromFile = cb => {
   });
 };
 
+
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id,title, imageUrl, description, price) {
+    this.id=id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,11 +28,38 @@ module.exports = class Product {
   }
 
   save() {
-    this.id=Math.random().toString();
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
+      if(this.id){
+        const existingProductIndex= products.findIndex(prod=> prod.id===this.id);
+        const updatedProducts=[...products];
+        updatedProducts[existingProductIndex]=this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      }
+      else{
+        this.id=Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
+        });
+      }
+      
+    });
+  }
+  static delete(id){
+    fs.readFile(p, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      let json = JSON.parse(data);
+      json = json.filter(obj => obj.id !== id);
+      fs.writeFile(p, JSON.stringify(json, null, 2), err => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('Object removed successfully');
+        }
       });
     });
   }
@@ -41,7 +70,7 @@ module.exports = class Product {
 
   static findById(id,cb){
     getProductsFromFile(products=>{
-      const product=products.find(p => p.id ===  id);
+      const product=products.find(p => p.id === id);
       cb(product);
     })
   }
